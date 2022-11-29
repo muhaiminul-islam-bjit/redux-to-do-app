f63cb4beab3efbc5717115714deccbd85f0c8549
muhaiminul-islam-bjit <muhaimiul.islam@bjitgroup.com>
2022-11-29 11:05:38 +0600

    first commit

---
+++ src/redux/todos/initialState.js
@@ -0,0 +1,13 @@
+export const initialState = [
+  {
+    id: 1,
+    text: "Learn with sumit",
+    completed: true,
+  },
+  {
+    id: 2,
+    text: "Learn redux",
+    completed: false,
+    color: "red",
+  },
+];
